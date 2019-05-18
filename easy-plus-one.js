de5e4343ee1505e9afd60b7ecc6789e16b2cb400
Lee <leetomko@gmail.com>
2019-05-17 20:26:29 -0400

    Create easy-plus-one.js

---
+++ easy-plus-one.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    
+    last = digits.length - 1;
+
+    digits[last] = digits[last] + 1;
+   
+   var i;
+    for (i = last; i >= 0; i--) { 
+       
+        if (digits[i] == 10) {
+            digits[i] = 0;
+            j = i - 1;
+            if (j >= 0){
+                digits[j] = digits[j] + 1;
+            } else {
+                digits.unshift(1);
+            }
+            
+        }
+        
+    }
+
+    
+    return digits;
+};
