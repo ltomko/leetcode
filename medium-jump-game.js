88b9527d5863752b7c957bccccd927e9f70066a4
Lee <leetomko@gmail.com>
2019-06-05 04:51:50 -0400

    Create medium-jump-game.js

---
+++ medium-jump-game.js
@@ -0,0 +1,20 @@
+var canJump = function(nums) {
+    
+    last = nums.length - 1;
+    max = 0;
+    
+    if (last == 0){ return true; }
+    
+    for (i=0; i <= last; i++){
+        if (nums[i] == 0 && i >= max){
+            return false;
+        }
+        if (nums[i] > 0 && nums[i] + i >= max){
+            max = nums[i] + i;
+            if (max >= last){ return true; }
+        }
+    }
+    
+    return true;
+
+};
